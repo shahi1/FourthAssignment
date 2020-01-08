a6e7b8dd8c348191d80ebf33128e7c1a265f326a
shahi1 <49000299+shahi1@users.noreply.github.com>
2020-01-08 23:22:07 +0545

    Signup Form beginning Design

--- app/src/main/java/com/example/fourthassignment/SignUpActivity.java
+++ app/src/main/java/com/example/fourthassignment/SignUpActivity.java
@@ -3,12 +3,35 @@ package com.example.fourthassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText PhoneNo;
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+       // PhoneNo = findViewById(R.id.etNo);
+        //ntnNext = findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(PhoneNo.getText().toString()))
+                {
+                    PhoneNo.setError("Mandatory Field");
+                    return;
+                }
+
+            }
+        });
+
     }
 }
